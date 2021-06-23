63e464652b9111c7275b9ca99aea98634b5ae58c
Jonas <jonas@wallmeier.name>
2021-06-23 14:56:03 +0200

    fix test script for reacher

--- test.py
+++ test.py
@@ -1,20 +1,31 @@
+import time
 import sys
-import gym
+from unityagents import UnityEnvironment
 from agent import Agent
 
-env = gym.make("Pendulum-v0")
+env = UnityEnvironment("Reacher_Linux_single/Reacher.x86_64")
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
 
-agent = Agent(3, 1)
+env_info = env.reset(train_mode=True)[brain_name]
+num_agents = len(env_info.agents)
+num_actions = brain.vector_action_space_size
+num_inputs = env_info.vector_observations.shape[1]
+
+seed = int(time.time() * 1000 % 1000)
+agent = Agent(n_inputs=num_inputs, n_actions=num_actions, random_seed=seed)
 agent.load_checkpoint(sys.argv[1])
 
-for i in range(50):
-    observation = env.reset()
+for i in range(10):
+    env_info = env.reset(train_mode=False)[brain_name]
+    state = env_info.vector_observations
     done = False
     score = 0
     while not done:
-        action = agent.act(observation, add_noise=False)
-        observation, reward, done, info = env.step(action)
-        score += reward
-        env.render()
+        action = agent.act(state, add_noise=False)
+        env_info = env.step(action)[brain_name]
+        state = env_info.vector_observations
+        score += env_info.rewards[0]
+        done = env_info.local_done[0]
     print(i + 1, score)
 env.close()
